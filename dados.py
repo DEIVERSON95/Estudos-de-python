160ef6d174af1caf1c3d59970a0c1fb4ea07f6d7
deiverson95 <deiverson455@gmail.com>
2020-06-15 22:49:05 -0300

    comados basicos de python

---
+++ dados.py
@@ -0,0 +1,4 @@
+nome = input('Qual seu nome? ')
+idade = int(input('Qual sua idade? '))
+endereco = input('Qual seu endereço? ')
+print(' Seu nome é: {} \n Sua idade é: {} anos \n Seu endereço é: {}'.format(nome,idade,endereco))
